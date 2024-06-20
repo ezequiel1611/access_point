035d56100a0d875f640ddd41399131602f732509
Ezequiel Ubiedo <lautaroezequielubiedo@gmail.com>
2024-06-20 20:17:58 -0300

    Se añadio un slider para controlar el nivel

--- data/script.js
+++ data/script.js
@@ -3,6 +3,14 @@ var websocket;
 // Init web socket when the page loads
 window.addEventListener('load', onload);
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+} 
 
 function onload(event) {
     initWebSocket();
